ce321b9a730937a609a4ae9e0675a9549f433bca
Nojan Sheybani <nojan@Nojans-MacBook-Pro.local>
2018-07-24 13:44:58 -0700

    v1 ready

--- gifMeSomeLovin/ViewController.swift
+++ gifMeSomeLovin/ViewController.swift
@@ -10,12 +10,34 @@ import UIKit
 import GiphyCoreSDK
 import FLAnimatedImage
 
+extension UIViewController {
+    func hideKeyboardWhenTappedAround() {
+        let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action: #selector(UIViewController.dismissKeyboard))
+        tap.cancelsTouchesInView = false
+        view.addGestureRecognizer(tap)
+    }
+    
+    @objc func dismissKeyboard() {
+        view.endEditing(true)
+    }
+}
+
 class ViewController: UIViewController {
     
     @IBOutlet weak var gif: FLAnimatedImageView!
     @IBOutlet weak var giffyLabel: UILabel!
     @IBOutlet weak var categoryStack: UIStackView!
     @IBOutlet weak var sendButton: UIButton!
+    @IBOutlet weak var textField: UITextField!
+    @IBOutlet weak var nojan: UILabel!
+    @IBOutlet weak var russell: UILabel!
+    @IBOutlet weak var joe: UILabel!
+    @IBOutlet weak var tommy: UILabel!
+    
+    var nojanScore = 0
+    var joeScore = 0
+    var russellScore = 0
+    var tommyScore = 0
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,6 +46,12 @@ class ViewController: UIViewController {
         giffyLabel.center.x = self.view.center.x
         gif.center.x = self.view.center.x
         sendButton.layer.cornerRadius = 5
+        self.hideKeyboardWhenTappedAround()
+        nojan.text = String(nojanScore)
+        joe.text = String(joeScore)
+        russell.text = String(russellScore)
+        tommy.text = String(tommyScore)
+        gif.image = #imageLiteral(resourceName: "theboys")
     }
 
     override func didReceiveMemoryWarning() {
@@ -32,7 +60,7 @@ class ViewController: UIViewController {
     }
     
     @IBAction func buttonPush(_ sender: Any) {
-        _ = GiphyCore.shared.random("power rangers") { (response, error) in
+        _ = GiphyCore.shared.random(textField.text!) { (response, error) in
             if let response = response, let data = response.data  {
                 print(response.meta)
                 print(data.url)
@@ -52,5 +80,27 @@ class ViewController: UIViewController {
             self.gif.animatedImage = imageData3
         }
     }
+    
+    @IBAction func nojanButtonPressed(_ sender: Any) {
+        nojanScore+=1
+        nojan.text = String(nojanScore)
+    }
+    
+    @IBAction func russellButtonPressed(_ sender: Any) {
+        russellScore+=1
+        russell.text = String(russellScore)
+    }
+    
+    @IBAction func joeButtonPressed(_ sender: Any) {
+        joeScore+=1
+        joe.text = String(joeScore)
+    }
+    
+    @IBAction func tommyButtonPressed(_ sender: Any) {
+        tommyScore+=1
+        tommy.text = String(tommyScore)
+    }
+    
+    
 }
 
